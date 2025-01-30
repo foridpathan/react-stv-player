47ccb588068fdfd7fe1247c94ba60de7abd9ad6a
Faridul Islam <foridpathan45@gmail.com>
2025-01-30 16:11:49 +0600

    demo deployment Add
    Signed-off-by: Faridul Islam <foridpathan45@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,14 @@
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { defineConfig } from "vite";
-import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
-import dtsPlugin from "vite-plugin-dts";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    cssInjectedByJsPlugin(),
-    react(),
-    dtsPlugin({
-      insertTypesEntry: true,
-      include: ["src/lib/**/*"],
-    }),
-  ],
+  plugins: [react()],
+  base: "./",
   build: {
-    lib: {
-      entry: resolve(__dirname, "src/lib/index.ts"),
-      name: "ReactSTVPlayer",
-      fileName: "react-stv-player",
-      formats: ["cjs", "es"]
-    },
-    sourcemap: true,
-    emptyOutDir: true,
+    outDir: "build",
     rollupOptions: {
-      external: ["react", "react-dom", "react/jsx-runtime"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-          "react/jsx-runtime": "react/jsx-runtime",
-        },
-      },
+      input: resolve(__dirname, "index.html"),
     },
   },
 });
